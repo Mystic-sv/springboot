3a3a7a04c7e0aea88c777093f868bae3fa554e06
Mystic-sv <122297557+Mystic-sv@users.noreply.github.com>
2024-03-27 15:18:25 +0530

    updates

--- bootdemowithjsp/src/main/java/com/techietact/controller/HomeController.java
+++ bootdemowithjsp/src/main/java/com/techietact/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.techietact.controller;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
